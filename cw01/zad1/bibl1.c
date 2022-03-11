04501a5d75ad0c8666e8eeb83762cd01c7027e2d
Milosz Dobosz <mdobosz@student.agh.edu.pl>
2022-03-11 09:56:00 +0100

    cw01/zad1

--- cw01/zad1/bibl1.c
+++ cw01/zad1/bibl1.c
@@ -1,20 +1,45 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
 #include "bibl1.h"
 
 void** allocate(int n) {
-	void** w = calloc(n, sizeof(void*));
+  if (n < 0) return NULL;
+
+	void** table = calloc(n, sizeof(void*));
 	printf("zarezerwowano");
-	return w;
+
+	return table;
 }
 
 void count(char* file) {
-	system(strcat(strcat("wc ", file), ">> temp"));
+  char* command = malloc(strlen(file) + 16);
+
+  strcat(command, "cat ");
+  strcat(command, file);
+  strcat(command, " | wc > temp");
+  system(command);
+
+  free(command);
+  printf("policzono");
 }
 
-void write() {
+void write_temp(void* buf) {
+  int file_descriptor = open("./temp", O_RDONLY);
+
+  struct stat file_stats;
+  fstat(file_descriptor, &file_stats);
+  int size = file_stats.st_size;
+  
+  read(file_descriptor, buf, size);
+  printf("zapisano");
 }
 
-void clear() {
+void clear(void** table, int index) {
+  free(table[index]);
+  printf("usunieto");
 }
--- cw01/zad1/bibl1.h
+++ cw01/zad1/bibl1.h
@@ -1,2 +1,5 @@
 #pragma once 
 extern void** allocate(int n);
+extern void count(char* file);
+extern void write_temp(void* buf);
+extern void clear(void** table, int index);

23ad3716cecdba9ba5a8c3e082e5413c6808f084
Milosz Dobosz <mdobosz@student.agh.edu.pl>
2022-03-15 22:21:28 +0100

    cw02/zad1 skończone linie

--- cw02/zad1/main.c
+++ cw02/zad1/main.c
@@ -1,43 +1,56 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#define BUFFER_SIZE 100
+#define PATH_SIZE 100
 
 int main(int argc, char* argv[]) {
   char* source_path;
   char* target_path;
 
+  FILE* source;
+  FILE* target;
+
   if (argc < 3) {
-    source_path = malloc(100);
-    target_path = malloc(100);
+    source_path[PATH_SIZE];
+    target_path[PATH_SIZE];
 
     printf("Podaj 2 pliki:\n");
     scanf("%s%s", source_path, target_path);
 
-    //TODO sprawdz nazwy plikow czy spoko
   } else {
     source_path = argv[1];
     target_path = argv[2];
   }
 
+  while(strcmp(source_path, target_path) == 0) {
+    printf("Podaj 2 poprawne pliki:\n");
+    scanf("%s%s", source_path, target_path);
+  } 
+
   printf("%s\n%s\n", source_path, target_path);
 
-  FILE* source = malloc(sizeof(FILE*));
-  FILE* target = malloc(sizeof(FILE*));
-
-  if ((source = fopen(source_path, "r")) == NULL || (source = fopen(target_path, "w")) == NULL) {
-    //error
-    printf("ERRRORRRR");
+  if ((source = fopen(source_path, "r")) == NULL || (target = fopen(target_path, "w")) == NULL) {
+    printf("Blad otwierania plikow!");
     return 1;
   }
 
-  //char* source_buffer = malloc(BUFFER_SIZE);
 
-  //while (fgets(source_buffer, BUFFER_SIZE, source) != NULL) {
-  //  printf("%s", source_buffer);
-  //}
-  
-  printf("alsdkfjlasdjf");
+  char buffer[BUFFER_SIZE];
 
-  fflush(stdout);
+  while (fgets(buffer, BUFFER_SIZE, source) != NULL) {
+    for (int i = 0; i < BUFFER_SIZE; i++) {
+      if (buffer[i] == '\n') {
+        break;
+      } else if (!isspace(buffer[i])) {
+        fprintf(target, "%s", buffer);
+        break;
+      }
+    }
+  }
+
   fclose(source);
   fclose(target);
 }

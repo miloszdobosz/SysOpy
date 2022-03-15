2da7921633a52c0dbb977a2d457bd07914f5a4a4
Milosz Dobosz <mdobosz@student.agh.edu.pl>
2022-03-15 17:20:58 +0100

    cw02/zad1 start serio

--- cw02/zad1/main.c
+++ cw02/zad1/main.c
@@ -1,16 +1,31 @@
+#include <stdlib.h>
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
-  char* source;
-  char* target;
+  char* source_path;
+  char* target_path;
+
   if (argc < 3) {
-    printf("Prosze podac 2 pliki:\n");
-    scanf("%s%s", source, target);
+    source_path = malloc(100);
+    target_path = malloc(100);
+
+    printf("Podaj 2 pliki:\n");
+    scanf("%s%s", source_path, target_path);
   } else {
-    source = argv[1];
-    target = argv[2];
+    source_path = argv[1];
+    target_path = argv[2];
   }
 
-  printf("%s", source);
-  printf("%s", target);
+  printf("%s\n%s\n", source_path, target_path);
+
+  FILE* source = malloc(sizeof(FILE*));
+  FILE* target = malloc(sizeof(FILE*));
+
+  if ((source = fopen(source_path, "r")) == NULL || (source = fopen(target_path, "w")) == NULL) {
+    //error
+    printf("ERRRORRRR");
+    return 1;
+  }
+
+
 }

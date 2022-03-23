589915b2c34ccba88002d22d0b838106e86277a2
Milosz Dobosz <mdobosz@student.agh.edu.pl>
2022-03-23 19:59:41 +0100

    cw03/zad2 dziala

--- cw03/zad2/integrate.c
+++ cw03/zad2/integrate.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/wait.h>
 
 int main (int argc, char *argv[]) {
   if (argc < 3) {
@@ -13,23 +14,25 @@ int main (int argc, char *argv[]) {
   int slices = atoi(argv[1]);
   int processes = atoi(argv[2]);
 
-  double dx = 1/slices;
+  double dx = 1.0/slices;
 
   for (int i = 0; i < processes; i++) {
     if (fork() == 0) {
       double area = 0.0; 
-      for (double x = i/processes; x < (i + 1)/processes; x += dx) {
-        area += 4/(x*x + 1);
+      for (double x = (double) i/processes; x < (double) (i + 1)/processes; x += dx) {
+        area += (4*dx)/(x*x + 1);
       }
 
-      char* number = itoa(i);
+      char number[10];
+      sprintf(number, "%d", i);
       char filename[5 + sizeof(number)];
+      filename[0] = '\0';
       strcat(filename, "w");
       strcat(filename, number);
       strcat(filename, ".txt");
 
       FILE* file = fopen(filename, "w");
-      fprintf(file, "%f", i, area);
+      fprintf(file, "%f", area);
       
       return 0;
     }

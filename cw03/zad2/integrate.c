72f98cd2f7dd578c72cfc03e10a840e712edee92
Milosz Dobosz <mdobosz@student.agh.edu.pl>
2022-03-23 20:26:42 +0100

    cw03/zad2 koniec

--- cw03/zad2/integrate.c
+++ cw03/zad2/integrate.c
@@ -4,6 +4,20 @@
 #include <string.h>
 #include <sys/wait.h>
 
+char* getFilename(int n) {
+  char number[10];
+  sprintf(number, "%d", n);
+
+  char* filename = malloc(5 + sizeof(number));
+  filename[0] = '\0';
+
+  strcat(filename, "w");
+  strcat(filename, number);
+  strcat(filename, ".txt");
+
+  return filename;
+}
+
 int main (int argc, char *argv[]) {
   if (argc < 3) {
     printf("Zbyt malo argumentow!\n");
@@ -19,27 +33,40 @@ int main (int argc, char *argv[]) {
   for (int i = 0; i < processes; i++) {
     if (fork() == 0) {
       double area = 0.0; 
+
       for (double x = (double) i/processes; x < (double) (i + 1)/processes; x += dx) {
         area += (4*dx)/(x*x + 1);
       }
 
-      char number[10];
-      sprintf(number, "%d", i);
-      char filename[5 + sizeof(number)];
-      filename[0] = '\0';
-      strcat(filename, "w");
-      strcat(filename, number);
-      strcat(filename, ".txt");
-
+      char* filename = getFilename(i);
       FILE* file = fopen(filename, "w");
       fprintf(file, "%f", area);
-      
+      free(filename);
+      fclose(file);
+
       return 0;
     }
   }
   
-  for (size_t i = 0; i < processes; i++) {
+
+  for (int i = 0; i < processes; i++) {
     wait(0);
   }
+
+  double area;
+  double total = 0.0;
+
+  for (int i = 0; i < processes; i++) {
+    char* filename = getFilename(i);
+    FILE* file = fopen(filename, "r");
+    fscanf(file, "%lf", &area);
+    free(filename);
+    fclose(file);
+
+    total += area;
+  }
+
+  printf("%f\n", total);
+
   return 0;
 }

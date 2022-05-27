562ea5143e001cfc92de9cdca4945f28de228055
Milosz Dobosz <mdobosz@student.agh.edu.pl>
2022-05-27 14:00:41 +0200

    szkielet

---
+++ cw09/mikolaj.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <time.h>
+
+void mikolaj()
+{
+}
+
+void renifer()
+{
+}
+
+void elf()
+{
+}
+
+int main(int argc, char const *argv[])
+{
+    pthread_t mikolaj;
+    pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
+
+    // mikolaj
+
+    for (int i = 0; i < 9; i++)
+    {
+        // renifery
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        // elfy
+    }
+
+    return 0;
+}
